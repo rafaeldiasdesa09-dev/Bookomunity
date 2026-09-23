879705edc67c5efd2adeca6236c769393da8f9cd
HEITOR JORGE MARQUES SANTOS <a95720527@rn.senac.local>
2026-09-23 14:18:50 -0300

    PageControllers.ts feito

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -3,3 +3,22 @@ import { Request, Response } from "express";
 export async function startPage(req: Request, res: Response) {
     res.render("index")
 }
+
+export async function loginPage(req: Request, res: Response) {
+    res.render("login")
+}
+
+
+export async function basePage(req: Request, res: Response) {
+    res.render("base")
+}
+
+
+export async function amizadesPage(req: Request, res: Response) {
+    res.render("friends")
+}
+
+
+export async function userPahe(req: Request, res: Response) {
+    res.render("user")
+}
